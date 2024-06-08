a900b2caa2ee6436b4081aeac43ea247829599ef
Saandeep Kotte <saandeepkotte5@gmail.com>
2024-06-08 18:15:05 +0530

    added comments for auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
+    // if no header at all => send header missing response
     if(!req.headers) res.status(404).json({message: "header missing"});
+
+    // check for authorization header
     const authHeader = req.headers['authorization'];
-    if(!authHeader) res.status(404).json({message: 'token missing...'});
+    if(!authHeader) res.status(404).json({message: 'token missing'});
+
+    // extract and verify token for accessing the route
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.JWT_SECRET, (err) => {
-        if(err) res.status(401).json({message: err.message});
+        if(err) res.status(403).json({message: err.message});
     });
     next();
 }
